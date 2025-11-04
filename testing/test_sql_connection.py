e6586e1d6438184e24be8fafb612e435e69209dd
Izma Khan <izmakhan2003@gmail.com>
2025-11-05 02:59:07 +0500

    database and sql connection tested

--- testing/test_sql_connection.py
+++ testing/test_sql_connection.py
@@ -3,8 +3,6 @@ Test database connection and basic queries
 """
 import sys
 import os
-
-# Add parent directory to path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from utils.db_connection import get_db
